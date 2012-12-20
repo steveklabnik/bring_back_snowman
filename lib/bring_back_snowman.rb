7e0aaedb463b778dfc25ac918eec90e5698d3291
Steve Klabnik <steve@steveklabnik.com>
2012-12-19 22:03:30 -0700

    Make it configurable.
    Thank you @drbrain for telling me about `.unpack("U")`. ❤

--- lib/bring_back_snowman.rb
+++ lib/bring_back_snowman.rb
@@ -1,10 +1,27 @@
+# encoding: UTF-8
+
 require "bring_back_snowman/version"
+
+module BringBackSnowman
+  class Railtie < Rails::Railtie
+   config.snowman = {}
+
+   config.after_initialize do |app|
+     app.config.snowman = {:_snowman => "☃"} if app.config.snowman == {}
+   end
+  end
+end
 
 module ActionView
   module Helpers
     module FormTagHelper
       def utf8_enforcer_tag
-        tag(:input, :type => "hidden", :name => "_snowman", :value => "&#9731".html_safe)
+        key = Rails.configuration.snowman.keys.first
+        value = Rails.configuration.snowman.values.first.unpack("U").first
+        tag(:input,
+            :type => "hidden",
+            :name => key,
+            :value => "&##{value}".html_safe)
       end
     end
   end
