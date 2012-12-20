dda51cd1f32334711fb5ad6117323dfc3346c955
Steve Klabnik <steve@steveklabnik.com>
2012-12-19 21:53:15 -0700

    Initial implementation.

--- lib/bring_back_snowman.rb
+++ lib/bring_back_snowman.rb
@@ -1,5 +1,11 @@
 require "bring_back_snowman/version"
 
-module BringBackSnowman
-  # Your code goes here...
+module ActionView
+  module Helpers
+    module FormTagHelper
+      def utf8_enforcer_tag
+        tag(:input, :type => "hidden", :name => "_snowman", :value => "&#9731".html_safe)
+      end
+    end
+  end
 end
